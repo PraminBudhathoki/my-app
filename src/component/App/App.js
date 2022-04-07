9c856bf9dc6a85e33373f38246f7f81b4ff05931
PraminBudhathoki <pramin2059@gmail.com>
2022-04-07 13:29:20 +0545

    usehook learned

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -2,14 +2,29 @@ import "./App.css";
 import Header from "../Header/Header";
 import SearchBar from "../Searchbar/Searchbar";
 import SearchHistory from "../Searchhistory/Searchhistory";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import SearchResults from "../SearchResults/SearchResults";
 
 export default function App() {
   const Name = "Company Name";
   const [terms, setTerms] = useState(["new hope", "empire"]);
+  const [films, setFilms] = useState([]);
 
-  function addTerm(term) {
-    setTerms([term, ...terms]);
+  function addTerm(terms) {
+    setTerms([terms, ...terms]);
+  }
+
+  useEffect(() => {
+    console.log("initial recnder complete");
+    fetchData("films");
+  }, []);
+
+  async function fetchData(type) {
+    let url = `https://swapi.dev/api/${type}`;
+    let resp = await fetch(url);
+    if (!resp.ok) throw new Error(resp.statusText);
+    let data = await resp.json();
+    setFilms(data.results);
   }
 
   // function myfunc(event) {
@@ -20,8 +35,11 @@ export default function App() {
   return (
     <div className="App">
       <Header company={Name} />
-      <SearchBar term={terms[0]} addTerm={addTerm} />
-      <SearchHistory term={terms} />
+      <SearchBar terms={terms[0]} addTerm={addTerm} />
+      <main className="content">
+        <SearchHistory terms={terms} />
+        <SearchResults films={films} />
+      </main>
     </div>
   );
 }
--- src/component/Searchhistory/Searchhistory.js
+++ src/component/Searchhistory/Searchhistory.js
@@ -2,7 +2,7 @@ import "./searchhistory.css";
 export default function SearchHistory(props) {
   return (
     <ul>
-      {props.term.map((terms) => (
+      {props.terms.map((terms) => (
         <li key={terms}>{terms}</li>
       ))}
     </ul>
