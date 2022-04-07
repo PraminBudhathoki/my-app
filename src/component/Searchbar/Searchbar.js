98867db911d3ccc6aad5e4bd52018d03c19ebb1e
PraminBudhathoki <pramin2059@gmail.com>
2022-04-07 10:16:57 +0545

    Episode-6 Completed

--- src/component/Searchbar/Searchbar.js
+++ src/component/Searchbar/Searchbar.js
@@ -1,13 +1,16 @@
 import "./searchbar.css";
-import { useState } from "react";
+// import { useState } from "react";
 
 export default function SearchBar(props) {
+  // const [term, setTerm] = useState("");
   // const [myprop, setMyProp] = useState("default value");
 
   function submitted(event) {
     event.preventDefault();
     console.log("submitted");
+    props.addTerm(event.target["keyword"].value);
     // setMyProp(event.target["keyword"].value);
+    // setTerm(event.target["keyword"].value);
   }
   function focused(event) {
     console.log("Focused");
@@ -39,7 +42,9 @@ export default function SearchBar(props) {
           Submit
         </button>
       </form>
-      <p>{myprop}</p>
+      {props.term ? <p>You searched for {props.term}</p> : ""}
+
+      {/* <p>{term}</p> */}
     </section>
   );
 }
